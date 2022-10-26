57361443f15f5f691bc3efb4b5f3d720655b2634
mzbxky <1376055937@qq.com>
2022-10-26 16:30:24 +0800

    山东码上Vue：数据汇总

--- src/report/summary.js
+++ src/report/summary.js
@@ -1,9 +1,20 @@
 //查询数据汇总
 import request from "@/utils/request";
 
+//每个应用每天的数据
 export function pageShow(query) {
   return request({
     url: '/Summary/pageShow',
+    method: 'get',
+    params: query
+  })
+}
+
+//每个用户每天的数据
+
+export function pageShowUser(query) {
+  return request({
+    url: '/Summary/pageShowUser',
     method: 'get',
     params: query
   })
--- src/views/report/summary.vue
+++ src/views/report/summary.vue
@@ -1,22 +1,12 @@
 <template>
 <div>
   <div>
-<el-form :inline="true" label-width="85px" style="margin-top: 15px">
-<!--  <el-form-item label="平台切换" >-->
-<!--    <el-select v-model="platform" placeholder="媒体" size="small" style="width: 200px">-->
-<!--      <el-option-->
-<!--        v-for="dict in platformList"-->
-<!--        :key="dict"-->
-<!--        :label="dict"-->
-<!--        :value="dict"-->
-<!--      />-->
-<!--    </el-select>-->
-<!--  </el-form-item> -->
+<el-form :inline="true" label-width="85px" style="margin-top: 10px">
   <el-form-item label="应用名称">
-    <el-input v-model="query.appName" placeholder="请输入应用名称" clearable size="small" style="width: 200px" @input="getList"/>
+    <el-input v-model="query.appName" placeholder="请输入应用名称" clearable size="small" style="width: 200px" @input="getList" :disabled="this.personalOrApp"/>
   </el-form-item>
   <el-form-item label="包名">
-    <el-input v-model="query.pkg" placeholder="请输入包名" clearable size="small" style="width: 200px" @input="getList"/>
+    <el-input v-model="query.pkg" placeholder="请输入包名" clearable size="small" style="width: 200px" @input="getList" :disabled="this.personalOrApp"/>
   </el-form-item>
   <el-date-picker
     v-model="value1"
@@ -25,15 +15,16 @@
     start-placeholder="开始日期"
     end-placeholder="结束日期"
     value-format="yyyy-MM-dd"
-    @change="getList()"
+    @change="serviceList()"
     :clearable="false"
     :picker-options="pickerOptions">
   </el-date-picker>
+  <el-button style="margin-left: 10px" @click="serviceButton"><span v-if="this.personalOrApp">应用总览</span><span v-if="!this.personalOrApp">个人总览</span></el-button>
 </el-form>
   </div>
-  <div>
-    <el-table v-loading="loading" :data="list" border style="padding-bottom: 50px" :height="tableHeight">
-      <el-table-column prop="time" label="日期" ></el-table-column>
+  <div v-if="!this.personalOrApp">
+    <el-table v-loading="loading" :data="list" border style="padding-bottom: 50px" :height="tableHeight" >
+      <el-table-column prop="time" label="日期" sortable></el-table-column>
       <el-table-column prop="nickName" label="用户"></el-table-column>
       <el-table-column prop="appName" label="应用">
         <template slot-scope="scope">
@@ -51,32 +42,50 @@
           </el-tooltip>
         </template>
       </el-table-column>
-      <el-table-column prop="cincome" label="平台c收益"></el-table-column>
-      <el-table-column prop="tincome" label="平台t收益"></el-table-column>
-      <el-table-column prop="total" label="总计"></el-table-column>
+      <el-table-column prop="cincome" label="平台c收益" sortable></el-table-column>
+      <el-table-column prop="tincome" label="平台t收益" sortable></el-table-column>
+      <el-table-column prop="total" label="总计" sortable></el-table-column>
     </el-table>
     <pagination v-show="total>0" :total="total" :page.sync="query.pageNum" :limit.sync="query.pageSize" @pagination="getList" style="bottom: 10px;right: 10px"/>
+  </div>
+  <div v-if="this.personalOrApp">
+    <el-table v-loading="loading" :data="listUser" border style="padding-bottom: 50px" :height="tableHeight" >
+      <el-table-column prop="time" label="日期" sortable></el-table-column>
+      <el-table-column prop="nickName" label="用户"></el-table-column>
+      <el-table-column prop="cincome" label="平台c收益" sortable>
+        <template slot-scope="scope">
+          {{scope.row.cincome}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="tincome" label="平台t收益" sortable>
+        <template slot-scope="scope">
+          {{scope.row.tincome}}
+        </template>
+      </el-table-column>
+      <el-table-column prop="total" label="总计" sortable></el-table-column>
+    </el-table>
+    <pagination v-show="total2>0" :total="total2" :page.sync="query2.pageNum" :limit.sync="query2.pageSize" @pagination="getListUser" style="bottom: 10px;right: 10px"/>
   </div>
 </div>
 </template>
 
 <script>
-import {selectPlatformName} from "@/report/platform";
-import {pageShow} from "@/report/summary"
+import {pageShow,pageShowUser} from "@/report/summary"
 
 export default {
   name: "summary",
   data(){
     return {
       tableHeight: "",//表格高度
+      //显示个人总览还是应用总览
+      personalOrApp:false,
       //获取数据条数
       total:0,
-      //平台绑定参数
-      platform:'穿山甲',
-      //平台
-      platformList:[],
+      total2:0,
       //获取到的数据
       list:[],
+      //获取到的数据
+      listUser:[],
       //向后端传递的查询数据
       query:{
         pageNum:1,
@@ -87,6 +96,12 @@ export default {
         endDate:null,
         //当前登录名称
         userName:"operating",
+      },
+      query2:{
+        pageNum:1,
+        pageSize:20,
+        startDate:null,
+        endDate:null,
       },
       pickerOptions: {
         shortcuts: [{
@@ -118,15 +133,42 @@ export default {
       value1:[],
     }
   },
+  mounted() {
+    //挂载window.onresize事件(动态设置table高度)
+    let _this = this;
+    window.onresize = () => {
+      if (_this.resizeFlag) {
+        clearTimeout(_this.resizeFlag);
+      }
+      _this.resizeFlag = setTimeout(() => {
+        _this.getTableHeight();
+        _this.resizeFlag = null;
+      }, 100);
+    };
+  },
   created() {
     // if (this.$store.state.user.name !== "admin"){
     //   this.userName = this.$store.state.user.name
     // }
     this.getTableHeight()
-    this.getPlatFormName()
-    this.getList()
+    this.serviceList()
   },
   methods:{
+    //控制列表显示
+    serviceList(){
+      if (this.personalOrApp === true){
+        //显示个人
+        this.getListUser()
+      }else {
+        //显示应用
+        this.getList()
+      }
+    },
+    //控制按钮
+    serviceButton(){
+      this.personalOrApp = this.personalOrApp !== true;
+      this.serviceList()
+    },
     //计算table高度(动态设置table高度)
     getTableHeight() {
       let tableH = 210; //距离页面下方的高度
@@ -161,16 +203,35 @@ export default {
       this.query.endDate = this.value1[1]
       this.loading = true
       pageShow(this.query).then(res=>{
-        this.total = res.data.total
-        this.list = res.data.list
+        if (res.code!==0){
+          this.$message({
+            message: res.msg,
+            type: 'error'
+          });
+        }else {
+          this.total = res.data.total
+          this.list = res.data.list
+        }
       })
       this.loading = false
     },
-    //查找平台
-    getPlatFormName(){
-      selectPlatformName().then(res=>{
-        this.platformList = res
+    //获取数据（用户）
+    getListUser(){
+      this.query2.startDate = this.value1[0]
+      this.query2.endDate = this.value1[1]
+      this.loading = true
+      pageShowUser(this.query2).then(res=>{
+        if (res.code!==0){
+          this.$message({
+            message: res.msg,
+            type: 'error'
+          });
+        }else {
+          this.total2 = res.data.total
+          this.listUser = res.data.list
+        }
       })
+      this.loading = false
     },
 }
 }
